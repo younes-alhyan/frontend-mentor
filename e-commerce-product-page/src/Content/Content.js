1df8bd887fa50956a5109668d0e5b55cce0a6eea
Yokai-47 <gameryounes555@gmail.com>
2025-08-13 21:08:16 +0100

    remove projects

--- e-commerce-product-page/src/Content/Content.js
+++
@@ -1,9 +0,0 @@
-import './Content.css'
-import Shoes from './Shoes'
-import StoreSection from './StoreSection'
-export default function Content({ addToCart, sneaker }) {
-    return (<div className="content">
-        <Shoes />
-        <StoreSection addToCart={addToCart} sneaker={sneaker}/>
-    </div>)
-}
