9dc6e321ae790be95d2ed1f5ca50c1f6c60a8575
Yokai47 <gameryounes555@gmail.com>
2025-09-28 18:32:48 +0100

    feat: add config file for paths

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+//config.js
+const path = require("path");
+
+const PROJECTS_DATA_FILE = path.resolve("../projects.json");
+const PROJECTS_FOLDER = path.resolve("../../");
+
+module.exports = {
+  PROJECTS_DATA_FILE,
+  PROJECTS_FOLDER,
+};
